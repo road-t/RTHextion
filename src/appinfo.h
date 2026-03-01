29e28eb3f0a020c43ad07bd709bd60aaae27c7a0
road-t <iannikov@gmail.com>
2026-03-01 22:46:49 +0300

    Aux stuff updated

--- src/appinfo.h
+++ src/appinfo.h
@@ -3,7 +3,7 @@
 
 namespace AppInfo {
 inline constexpr const char* Name = "RTHextion";
-inline constexpr const char *Version = "0.9";
+inline constexpr const char *Version = "1.0 alpha";
 }
 
 #endif
