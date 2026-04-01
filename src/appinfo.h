72fe8420bd418cb098a326891ae314246e84dc10
road-t <iannikov@gmail.com>
2026-04-02 00:04:37 +0300

    Version bumped

--- src/appinfo.h
+++ src/appinfo.h
@@ -3,7 +3,7 @@
 
 namespace AppInfo {
 inline constexpr const char* Name = "RTHextion";
-inline constexpr const char *Version = "2.0 beta 1";
+inline constexpr const char *Version = "2.0 beta 2";
 }
 
 #endif
